cef53693ec0b8990e8ab5368444cd96e6624bc93
Ivan Karshev <93340934+IKarshev@users.noreply.github.com>
2023-11-21 10:32:36 +0700

    init select2, IK_js_helper.js

--- include_lib/IncludeLib.php
+++ include_lib/IncludeLib.php
@@ -3,6 +3,7 @@
 <? $APPLICATION->SetAdditionalCss(SITE_TEMPLATE_PATH . "/css/slick-theme.css");?>
 <? $APPLICATION->SetAdditionalCss(SITE_TEMPLATE_PATH . "/css/slick.css");?>
 <? $APPLICATION->SetAdditionalCss(SITE_TEMPLATE_PATH . "/css/slick-animation.min.js");?>
+<? $APPLICATION->SetAdditionalCss(SITE_TEMPLATE_PATH . "/css/select2.min.css");?>
 
 <!-- js -->
 <?CJSCore::Init(array("jquery"));?>
@@ -10,3 +11,7 @@
 <? $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/jquery.fancybox.min.js");?>
 <? $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/jquery.validate.min.js");?>
 <? $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/jquery.inputmask.min.js");?>
+<? $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/select2.min.js");?>
+
+<!-- IK js helper -->
+<? $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/IK_js_helper.js");?>
